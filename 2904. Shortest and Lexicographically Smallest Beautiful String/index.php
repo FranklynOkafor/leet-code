b3bc753fc6377221b6fb084c9596ce23b79aa7a5
Franklyn Okafor <frankeokafor@gmail.com>
2026-08-26 21:05:43 +0100

    feat: Final implementation:  shortestBeautifulSubstring solution using sliding window of indices

--- 2904. Shortest and Lexicographically Smallest Beautiful String/index.php
+++ 2904. Shortest and Lexicographically Smallest Beautiful String/index.php
@@ -3,7 +3,6 @@
 
 class Solution
 {
-
     /**
      * @param String $s
      * @param Integer $k
@@ -14,69 +13,54 @@ class Solution
         $k = (int)$k;
         $s_array = str_split($s);
 
+        // Get the positions of all 1s
         $ones = array_keys($s_array, "1");
+
+        // Not enough 1s to make a beautiful substring
         if (count($ones) < $k) {
             return "";
         }
 
+        $shortest_count = PHP_INT_MAX;
+        $shortest_sub = "";
+
+        // Each group of k consecutive 1s gives us a candidate
         $break_num = count($ones) - $k;
 
-        $shortest_count = 100;
-        $shortest_sub = "";
-        $array_of_shortests = [];
         for ($i = 0; $i <= $break_num; $i++) {
+
+            // Get the positions of the current k 1s
             $current_ones = array_slice($ones, $i, $k);
-            $current_count = $current_ones[count($current_ones) - 1] - $current_ones[0] + 1;
 
+            // Length of the substring from first 1 to last 1
+            $current_count =
+                $current_ones[$k - 1] - $current_ones[0] + 1;
 
+            // Get the actual substring
+            $current_sub = substr(
+                $s,
+                $current_ones[0],
+                $current_count
+            );
+
+            // Found a shorter substring
             if ($current_count < $shortest_count) {
                 $shortest_count = $current_count;
-                $shortest_sub = substr($s, $current_ones[0], $current_count);
-
-                array_push($array_of_shortests, $shortest_sub);
-               
+                $shortest_sub = $current_sub;
             }
 
-            if (count($array_of_shortests) > 1) {
-                if (count($array_of_shortests) > 1) {
-                    $longestIndex = 0;
-                    $longestLength = strlen($array_of_shortests[0]);
-
-                    foreach ($array_of_shortests as $index => $value) {
-                        if (strlen($value) > $longestLength) {
-                            $longestLength = strlen($value);
-                            $longestIndex = $index;
-                        }
-                    }
-
-                    unset($array_of_shortests[$longestIndex]);
-                    $array_of_shortests = array_values($array_of_shortests); // reindex
-                }
+            // Same length: choose lexicographically smaller
+            elseif (
+                $current_count == $shortest_count &&
+                $current_sub < $shortest_sub
+            ) {
+                $shortest_sub = $current_sub;
             }
-            // print_r($array_of_shortests);
         }
 
-        $highest_indices_sum = 0;
-        if (count($array_of_shortests) > 1){
-            foreach ($array_of_shortests as $current_sub) {
-                $current_indices_sub = array_keys(str_split($current_sub), "1");
-                $current_indices_sum = array_sum($current_indices_sub);
-                if ($current_indices_sum > $highest_indices_sum){
-                    $highest_indices_sum = $current_indices_sum;
-                    $shortest_sub = $current_sub;
-                }
-
-            }
-
-            return $shortest_sub;
-
-        }
-        else {
-            return $array_of_shortests[0];
-        }
+        return $shortest_sub;
     }
 }
-
 
 $result = new Solution();
 
